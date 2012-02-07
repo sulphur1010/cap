55202f1a3f3aab3ae5f07bd467c5f6c6ec6d0b01
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-02-07 13:50:08 -0500

    added a table for the menu item section along with indenting.  the weight column is used everywhere now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207050709) do
+ActiveRecord::Schema.define(:version => 20120207183649) do
 
   create_table "content_fragments", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20120207050709) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "weight"
   end
 
   add_index "menu_items", ["parent_id"], :name => "index_menu_items_on_parent_id"
