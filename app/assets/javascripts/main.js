0a4b386f18731b7400f361820f0a7dfcc9953299
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-02-08 11:13:58 -0500

    added the contemporary_issues selection to the person_type form.  added the contemporary_issues to the bottom of the person_type view and added a basic contemporary_issues view page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 $(function() {
-  $('textarea.editor').tinymce({ theme: 'advanced' });
+  $('textarea.js_editor').tinymce({ theme: 'advanced' });
+  $('select.js_multiselect').multiSelect();
 
   $(".formtastic fieldset legend span").click(function() {
     elements = $(this).parents("legend").siblings("ol");
