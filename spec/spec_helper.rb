6511b9c4fa7723af7ec086dd2a097f28acb2f872
Darrik Mazey <darrik@darmasoft.com>
2012-02-09 11:24:31 -0500

    move blueprints where they go

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require File.expand_path(File.dirname(__FILE__) + '/../test/blueprints')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -11,3 +11,51 @@ require 'machinist/active_record'
 MenuItem.blueprint do
   # Attributes here
 end
+
+# Blocks
+Block.blueprint do
+	name { "Random Block" }
+end
+
+# Contemporary Issues
+ContemporaryIssue.blueprint do
+end
+
+# Content Fragments
+ContentFragment.blueprint do
+	title { "test content fragment" }
+	url { "/some/other/url" }
+end
+
+# Pages
+Page.blueprint do
+	title { "test page" }
+	url { "/some/url" }
+	body { "Lorem ipsum..." }
+end
+
+Page.blueprint(:no_slash) do
+	url { "no/initial/slash" }
+end
+
+# Person Types
+PersonType.blueprint do
+end
+
+# Prism Types
+PrismType.blueprint do
+end
+
+# Role Types
+RoleType.blueprint do
+end
+
+# Thoughts
+Thought.blueprint do
+end
+
+# Users
+User.blueprint do
+	email { "test@test.com" }
+	password { "testtest" }
+end
