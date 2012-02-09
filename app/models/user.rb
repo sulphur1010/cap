7ce50cb1f5947c501be0a0ca121c0cabae55d47b
Darrik Mazey <darrik@darmasoft.com>
2012-02-09 13:28:11 -0500

    whitelist fields for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :title, :phone, :chapter_id
 
   has_many :content_fragments
 	belongs_to :chapter
