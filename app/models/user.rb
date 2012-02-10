6af1db1ee525b1613339e169a756ab04083a91a9
Darrik Mazey <darrik@darmasoft.com>
2012-02-10 13:18:18 -0500

    convert role_list to roles array; gatekeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,59 @@ class User < ActiveRecord::Base
   has_many :content_fragments
 	belongs_to :chapter
 
+	after_initialize :load_roles
+	before_save :convert_roles
+
+	def has_role?(r)
+		normalize_roles
+		@roles.include?(r) or @roles.include?(r.to_s)
+	end
+
+	def roles=(r)
+		if r.class == Array
+			@roles = r.map { |r| r.to_s }
+		else
+			@roles = [r.to_s] unless r.to_s.blank?
+		end
+		normalize_roles
+		@roles
+	end
+
+	def add_role!(r)
+		if r.class == Array
+			r.each do |role|
+				self.add_role!(role)
+			end
+		else
+			@roles << r.to_s unless r.to_s.blank?
+		end
+		normalize_roles
+		@roles
+	end
+
+	def roles
+		unless @roles
+			@roles = self.role_list.split(/,/).map { |r| r.strip } rescue []
+		end
+		normalize_roles
+		@roles
+	end
+
+	private
+
+	def normalize_roles
+		@roles = @roles.map { |r| r.to_s }
+	end
+
+	def load_roles
+		@roles = self.role_list.split(/,/).map { |r| r.strip } rescue []
+		unless @roles
+			@roles = []
+		end
+	end
+
+	def convert_roles
+		self.role_list = @roles.map { |r| r.to_s }.join(',')
+	end
+
 end
