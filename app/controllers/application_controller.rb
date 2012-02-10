46b1d70c5a8b3e72129b0f0eed595c599981673f
Darrik Mazey <darrik@darmasoft.com>
2012-02-10 13:26:33 -0500

    application controller has_role?(r) helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	def has_role?(r, &block)
+		if block_given?
+			yield if current_user && current_user.has_role?(r)
+		else
+			if current_user && current_user.has_role?(r)
+				return true
+			else
+				return false
+			end
+		end
+	end
+
 end
