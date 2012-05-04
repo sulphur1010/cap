e282025ae9fb2326545f2fd852e3d6c6a6f6828b
Darrik Mazey <darrik@darmasoft.com>
2012-05-04 13:36:49 -0400

    arbitrary menu system

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -6,5 +6,25 @@ class MenuItem < ActiveRecord::Base
 		[ "Top", "About" ]
 	end
 
+	def self.all_with_relative_depth
+		MenuItem.where(:parent_id => nil).all.map { |m| [m, m.all_children] }.flatten
+	end
+
+	def all_children
+		MenuItem.where(:parent_id => self).map { |m| [m, m.all_children] }.flatten
+	end
+
+	def menu_depth
+		if self.parent
+			self.parent.menu_depth + 1
+		else
+			0
+		end
+	end
+
+	def parent_menu_name_with_depth
+		"#{" - " * self.menu_depth}#{self.name}"
+	end
+
 	validates :menu, :inclusion => { :in => MenuItem.menus }
 end
