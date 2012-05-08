c6421050a0d6add7ed7a4e358fa81bf14a59530d
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-05-08 12:58:41 -0400

    the answer popup now shows up in a position relative to the question button

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,7 +2,11 @@ $(function() {
 	$(document).delegate('.answer_button', 'click', function() {
 		var qid = $(this).attr('data-id');
 		$('.answer_popup').hide();
-		$('.answer_popup[data-id="' + qid + '"]').show();
+		var answer = $('.answer_popup[data-id="' + qid + '"]')
+		var pos = $(this).position();
+		answer.attr('style', 'top:' + pos.top + 'px');
+		answer.show();
+		
 	});
 
 	$(document).delegate('.answer_popup .answer_close', 'click', function() {
