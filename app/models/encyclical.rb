7977e7e272f4b7b88c85656ddd59602fb0122e1a
Darrik Mazey <darrik@darmasoft.com>
2012-09-10 13:42:41 -0400

    reference keyword function for encyclicals

--- app/models/encyclical.rb
+++ app/models/encyclical.rb
@@ -2,4 +2,9 @@ class Encyclical < ContentFragment
 	validates :title, :presence => true
 	validates :name, :presence => true
 	validates :body, :presence => true
+
+	def reference_keyword
+		self.title.titleize.gsub(/[^A-Z]/, '')
+	end
+
 end
