89ea656b76799be57e4ed7492ea6c46870982857
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-09-11 14:14:52 -0400

    finished the tinymce popup for encyclical references

--- app/models/encyclical.rb
+++ app/models/encyclical.rb
@@ -3,8 +3,18 @@ class Encyclical < ContentFragment
 	validates :name, :presence => true
 	validates :body, :presence => true
 
+	before_save :parse_chapters
+
 	def reference_keyword
 		self.title.titleize.gsub(/[^A-Z]/, '')
+	end
+
+	def parse_chapters
+		self.body.split(/<br \/>(?=\d+\. )/).map {|chapter|
+			number = chapter.split(".").first
+			number = 0 if number.to_i.to_s != number
+			"<a name='chapter_#{number}'></a><p class='chapter' id='chapter_#{number}' data-id='#{number}'>#{chapter}</p>"
+		}.join("")
 	end
 
 end
