173c53a3206951884a29c723ac53c3f993b86176
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-09-16 18:48:05 -0400

    added a boat load of payment confirmation fields

---
+++ db/migrate/20120916224332_add_more_payment_confirmations_fields.rb
@@ -0,0 +1,27 @@
+class AddMorePaymentConfirmationsFields < ActiveRecord::Migration
+  def up
+		add_column :payment_confirmations, :item_taxable1, :string
+		add_column :payment_confirmations, :item_isbn1, :string
+		add_column :payment_confirmations, :item_plu1, :string
+		add_column :payment_confirmations, :item_style_number1, :string
+		add_column :payment_confirmations, :item_tax_rate_double1, :string
+		add_column :payment_confirmations, :tax1, :string
+		add_column :payment_confirmations, :item_tax_rate1, :string
+		add_column :payment_confirmations, :item_count_unit1, :string
+		add_column :payment_confirmations, :item_mpn1, :string
+		add_column :payment_confirmations, :item_model_number1, :stringS
+  end
+
+  def down
+		remove_column :payment_confirmations, :item_taxable1
+		remove_column :payment_confirmations, :item_isbn1
+		remove_column :payment_confirmations, :item_plu1
+		remove_column :payment_confirmations, :item_style_number1
+		remove_column :payment_confirmations, :item_tax_rate_double1
+		remove_column :payment_confirmations, :tax1
+		remove_column :payment_confirmations, :item_tax_rate1
+		remove_column :payment_confirmations, :item_count_unit1
+		remove_column :payment_confirmations, :item_mpn1
+		remove_column :payment_confirmations, :item_model_number1
+  end
+end
