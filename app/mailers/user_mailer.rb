2d9ea124445dd31a01e4b895f30a4a74b8342059
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-09-17 12:34:44 -0400

    fixed some bugs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,25 @@ class UserMailer < ActionMailer::Base
 
 	def reminder_email(er, user)
 	end
+
+	def event_registered_user(event, user)
+		@event = event
+		@user = user
+		mail(:to => user.email, :subject => "You have registered for a CAPP-USA event!")
+	end
+
+	def event_daily_summary(event, users)
+		@event = event
+		@users = users
+		@director = @event.director
+		@program_contact = "drkwright@capp-usa.org"
+		cc = nil
+		if @director && @director.email != @program_contact
+			cc = @director.email
+		end
+		mail(:to => @program_contact, :cc => cc, :subject => "#{@event.title} - Contact List (Daily Summary)")
+	end
+
+	def event_reminder_user(event, user)
+	end
 end
