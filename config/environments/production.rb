2182324ce1d5960f62664c91cec924184317168c
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-09-18 15:55:31 -0400

    added production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,15 @@ CappUsa::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'http://capp-usa.org' }
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		:address              => "smtp.gmail.com",
+		:port                 => 587,
+		:domain               => 'capp-usa.org',
+		:user_name            => 'website-notifications',
+		:password             => 'webadmincapp2012',
+		:authentication       => 'plain',
+		:enable_starttls_auto => true
+	}
 end
