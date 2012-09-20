fb832ab8289bd8cf574729e3822339f1144d91b3
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-09-20 16:12:06 -0400

    updates

--- app/models/development_mail_interceptor.rb
+++ app/models/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "liam@trueinteraction.com"
+    message.to = "developer@trueinteraction.com"
   end
 end
