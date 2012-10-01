be4079655747bbb3dd809a8754d154a26d8dc4e4
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-10-01 14:24:18 -0400

    admin manually adding people to an event

--- app/controllers/admin/attendees_events_controller.rb
+++ app/controllers/admin/attendees_events_controller.rb
@@ -4,6 +4,19 @@ class Admin::AttendeesEventsController < ApplicationController
 
 	respond_to :html
 
+	def create
+		@event = Event.find(params[:event_id])
+		@attendee_event = AttendeesEvent.new(params[:attendees_event])
+		@attendee_event.event = @event
+		if @attendee_event.save
+			flash[:notice] = "User added to event"
+			redirect_to event_path(@event)
+		else
+			flash[:notice] = "Error adding user to event"
+			redirect_to event_path(@event)
+		end
+	end
+
 	def destroy
 		@event = Event.find(params[:event_id])
 		@attendee_event = AttendeesEvent.find(params[:id])
