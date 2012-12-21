4bc934a1df59a81e1b609c1878d476e09b304471
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-12-21 17:12:19 -0500

    fixed another migration issue

--- db/migrate/20121221184815_change_email_list_field_to_true_for_users.rb
+++ db/migrate/20121221184815_change_email_list_field_to_true_for_users.rb
@@ -1,10 +1,6 @@
 class ChangeEmailListFieldToTrueForUsers < ActiveRecord::Migration
   def up
 		change_column :users, :email_list, :boolean, :default => true
-		User.all.each do |user|
-			user.email_list = true
-			user.save!
-		end
   end
 
   def down
