c3bb4a744f6e57be1ac76093d141f33ca179b8e5
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2012-12-21 17:01:40 -0500

    fixed a migration

--- db/migrate/20121221190236_rename_about_us_type_for_users.rb
+++ db/migrate/20121221190236_rename_about_us_type_for_users.rb
@@ -1,10 +1,6 @@
 class RenameAboutUsTypeForUsers < ActiveRecord::Migration
   def up
 		rename_column :users, :about_us_type, :affiliation_list
-		User.all.each do |user|
-			user.affiliations = []
-			user.save!
-		end
   end
 
   def down
