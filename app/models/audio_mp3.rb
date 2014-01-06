c91895681ef380c6daca60f13a539f99e5fc817c
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2014-01-06 16:55:18 -0500

    changed the path for the audio mp3s to fit better with the deploy

--- app/models/audio_mp3.rb
+++ app/models/audio_mp3.rb
@@ -1,7 +1,7 @@
 class AudioMp3 < ActiveRecord::Base
   belongs_to :audio_content
   attr_accessible :audio_mp3_content_type, :audio_mp3_file_size, :audio_mp3_updated_at, :audio_mp3_file_name, :audio_content_id, :audio_title, :audio_mp3
-  has_attached_file :audio_mp3, :url => "/:class/:id/:filename", :path => ":rails_root/public/:class/:id/:filename"
+  has_attached_file :audio_mp3, :url => "/system/:class/:id/:filename", :path => ":rails_root/public/system/:class/:id/:filename"
   validates_format_of :audio_mp3_file_name, :with => %r{\.(mp3)$}i, :message => 'should be a mp3 file'
 end
 
