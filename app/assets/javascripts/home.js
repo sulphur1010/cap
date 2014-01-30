299bb79185d4814270fbfa7bdf16fee9d2aeedf3
O. Liam Wright <liam@trueinteraction.com>
2014-01-30 10:54:00 -0500

    modded comms methods

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,7 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+var Notice = new function() {
+
+	this.show_notice = function(message) {
+		$('#notice').html(message);
+		$('#notice').slideDown('slow').delay(3000).slideUp('slow');
+	};
+};
