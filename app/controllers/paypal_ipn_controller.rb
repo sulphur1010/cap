af79e8fa398cf17d04620a0cf8d64e8269bb80af
Matthew Tidd (DMT Key) <matt@dmtprogramming.com>
2014-07-25 08:11:31 -0400

    fixed a bug with the paypal ipn code

--- app/controllers/paypal_ipn_controller.rb
+++ app/controllers/paypal_ipn_controller.rb
@@ -13,21 +13,21 @@ class PaypalIpnController < ApplicationController
 		invoice = params[:invoice]
 		pc = PaymentConfirmation.update(invoice, params)
 
-		pc.attendee_event.first_name = pc.first_name
-		pc.attendee_event.last_name = pc.last_name
-		pc.attendee_event.email = pc.payer_email
-		pc.attendee_event.address = pc.address_street
-		pc.attendee_event.city = pc.address_city
-		pc.attendee_event.zip = pc.address_zip
-		pc.attendee_event.guest_name = session[:guest_name]
+		pc.attendees_event.first_name = pc.first_name
+		pc.attendees_event.last_name = pc.last_name
+		pc.attendees_event.email = pc.payer_email
+		pc.attendees_event.address = pc.address_street
+		pc.attendees_event.city = pc.address_city
+		pc.attendees_event.zip = pc.address_zip
+		pc.attendees_event.guest_name = session[:guest_name]
 		session[:guest_name] = nil
-		pc.attendee_event.save
+		pc.attendees_event.save
 		unless pc.save
 			puts pc.errors
 		end
 
 		UserMailer.event_registered_user(pc.event, pc.attendees_event).deliver
-		UserMailer.event_registered_admin(pc.event, pc.attendee_event, pc).deliver
+		UserMailer.event_registered_admin(pc.event, pc.attendees_event, pc).deliver
 		render :nothing => true
 	end
 end
