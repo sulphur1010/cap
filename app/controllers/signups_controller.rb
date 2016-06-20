af286bcdc0324ef93d1335586985bef2cc94cb9b
Jake Christensen <jakec43@gmail.com>
2016-06-20 00:56:10 -0400

    error message

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -6,15 +6,15 @@ class SignupsController < ApplicationController
 
 	def create
 		@signup = Signup.new(params[:signup])
-		success, id = Signup.payment(params[:credit_card], @signup)
+		success, @id = Signup.payment(params[:credit_card], @signup)
 		if success
-			@signup.paypal_id = id
+			@signup.paypal_id = @id
 			@signup.save
 			UserMailer.signup_confirm(@signup).deliver
 			UserMailer.signup_user_confirm(@signup).deliver
 			redirect_to confirm_signups_path 
 		else
-			raise "Something went wrong"
+			error
 		end
 	end
 	
@@ -22,11 +22,11 @@ class SignupsController < ApplicationController
 	end
 	
 	def error
-		@hash = JSON.parse(@signup.paypal_id)
-		if @hash[:name] == "CREDIT_CARD_REFUSED"
+		if @id[:name] == "CREDIT_CARD_REFUSED"
 			@error = "Your credit card was declined. Please check the details you entered and try again. We apologize for the inconvenience."
 		else
 			@error = "An unknown error occured while attempting to process your credit card. Please check the details you entered and try again. We apologize for the inconvenience."
 		end
+		render :action => :error
 	end
 end
