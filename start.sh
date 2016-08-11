e8f1926c508a57c14e6eadfecdb44ac36ad8bcdd
Greg Madison <greg.madison@startmail.com>
2016-08-11 16:09:18 -0400

    delayed job to start.sh

--- start.sh
+++ start.sh
@@ -23,8 +23,12 @@ bundle exec rake db:seed RAILS_ENV=$RAILS_ENV
 echo "** Starting solr **"
 bundle exec rake sunspot:solr:start RAILS_ENV=$RAILS_ENV
 
+echo "** Compiling assets **"
+bundle exec rake assets:clean
+bundle exec rake assets:precompile
+
 echo "** Starting Unicorn **"
 bundle exec unicorn_rails -c $WEBDIR/$REPO/config/unicorn-capp.rb -E $RAILS_ENV -D
 
 echo "** Starting Delayed Job **"
-bundle exec bin/delayed_job start RAILS_ENV=$RAILS_ENV
+bundle exec rake jobs:work RAILS_ENV=$RAILS_ENV
