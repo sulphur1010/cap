77c3d0bc1e46e93bb6972ab2327dc1a2e05c9f85
= <=>
2021-10-14 05:00:13 +0100

    another patch

--- config/initializers/patch.rb
+++ config/initializers/patch.rb
@@ -30,3 +30,22 @@ class Integer
   end
   alias :year :years
 end
+
+
+
+
+module Arel
+  module Visitors
+    class DepthFirst < Arel::Visitors::Visitor
+      alias :visit_Integer :terminal
+    end
+
+    class Dot < Arel::Visitors::Visitor
+      alias :visit_Integer :visit_String
+    end
+
+    class ToSql < Arel::Visitors::Visitor
+      alias :visit_Integer :literal
+    end
+  end
+end
